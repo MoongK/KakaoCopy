c6812b9b054c2cc3fdec320f05875f587cb9b375
MoongK <qorfyd01@gmail.com>
2018-03-22 21:05:05 +0900

    first..-from dga

---
+++ Assets/Resources/SampleButton.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.UI;
+
+public class SampleButton : MonoBehaviour {
+
+    public Text nameLabel;
+    public Image iconImage;
+    public Text priceText;
+
+    Item item;
+    ShopScrollList scrollList;
+
+    public void Setup(Item currentItem, ShopScrollList currentScrollist)
+    {
+        item = currentItem;
+        nameLabel.text = item.itemName;
+        iconImage.sprite = item.icon;
+        priceText.text = item.price.ToString() + " $ ";
+        scrollList = currentScrollist;
+
+        GetComponent<Button>().onClick.AddListener(HandleClick);
+    }
+
+    private void HandleClick()
+    {
+        print("clicked!");
+        scrollList.TryTransferItemToOtherShop(item);
+    }
+}
